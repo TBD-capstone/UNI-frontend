b7fc8227892df91bc6f16f3dd56fa7310a654756
unknown <han1267@ajou.ac.kr>
2024-11-18 04:37:59 +0900

    [HOTFIX2]

--- uni_frontend/src/navbar.js
+++ uni_frontend/src/navbar.js
@@ -34,7 +34,7 @@ function Navbar({ selectedLanguage, fetchWithLanguage }) {
     const handleLogout = () => {
         document.cookie = 'userName=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
         document.cookie = 'userId=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-        document.cookie = 'language=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+        /*document.cookie = 'language=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';*/
         navigate('/login');
     };
 
