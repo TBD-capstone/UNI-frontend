c6cf60a2f580ccb97b5d6c303d88e91dfecfc9a7
unknown <han1267@ajou.ac.kr>
2024-11-27 22:57:36 +0900

    [HOTFIX]

--- uni_frontend/src/navbar.js
+++ uni_frontend/src/navbar.js
@@ -110,11 +110,7 @@ function Navbar({ selectedLanguage, fetchWithLanguage }) {
                 </div>
                 <div className="user">
                     {/* 프로필 이미지 로드 */}
-                    <img
-                        src={profileImage || './profile-image.png'} // 기본 프로필 이미지 설정
-                        alt="프로필"
-                        className="profile-image"
-                    />
+                    <img src={profileImage} alt="프로필" className="profile-image" />
                 </div>
                 <div className="userid">
                     <Link to={`/user/${userId}`} className="username-link">
