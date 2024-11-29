c76214bc596914c9b0d5ad6fb7b4887b1074937d
jaeyong <ljy9085@ajou.ac.kr>
2024-11-29 21:21:17 +0900

    [HOTFIX] 정렬 리스트 번역 키 추가 작성

--- uni_frontend/src/mainpage.js
+++ uni_frontend/src/mainpage.js
@@ -137,9 +137,9 @@ const ProfileGrid = () => {
                     />
                     <button>{t('mainpage.search_button')}</button>
                     <select onChange={handleSortChange} value={sortOrder}>
-                        <option value="newest">{t('최신순')}</option>
-                        <option value="highest_rating">{t('별점 높은 순')}</option>
-                        <option value="lowest_rating">{t('별점 낮은 순')}</option>
+                        <option value="newest">{t('mainpage.newest')}</option>
+                        <option value="highest_rating">{t('mainpage.highest_rating')}</option>
+                        <option value="lowest_rating">{t('mainpage.lowest_rating')}</option>
                     </select>
                 </div>
             </div>
@@ -175,7 +175,7 @@ const ProfileGrid = () => {
                         </Link>
                     ))
                 ) : (
-                    <div className="no-profiles">{t('mainpage.noProfiles')}</div>
+                    <div className="no-profiles"> </div>
                 )}
             </div>
 
