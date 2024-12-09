c53ab4eb903f4ab474d93e566b07a76eae8b831b
한성재 <nicola1928@ajou.ac.kr>
2024-12-09 15:43:42 +0900

    [FIX] userpage isKorean 개선

--- uni_frontend/src/pages/user-page/UserPage.js
+++ uni_frontend/src/pages/user-page/UserPage.js
@@ -22,6 +22,7 @@ const UserPage = () => {
     const navigate = useNavigate();
     const {userId} = useParams();
     const [isKorean, setIsKorean] = useState(false);
+    const [isKoreanMe, setIsKoreanMe] = useState(false);
     const [user, setUser] = useState(null);
     const [markers, setMarkers] = useState(null);
     const [activeTab, setActiveTab] = useState('Qna');
@@ -59,7 +60,7 @@ const UserPage = () => {
                     </> :
                     <>
                         <div>{t('userPage.chat_explain1')}{props.userName}{t('userPage.chat_explain2')}</div>
-                        <button className="Chatting" onClick={handleClickChat}>{t('userPage.chat')}</button>
+                        {!props.isKoreanMe && <button className="Chatting" onClick={handleClickChat}>{t('userPage.chat')}</button>}
                         <button className="Report" onClick={handleClickReport(userId)}>{t('userPage.report')}</button>
                     </>
                 }
@@ -323,7 +324,7 @@ const UserPage = () => {
             await getMyData().then((data) => {
                 setCommenterId(() => data.userId);
                 const isKorean = data.role !== 'EXCHANGE';
-                setIsKorean(() => isKorean);
+                setIsKoreanMe(() => isKorean);
                 console.log(data);
             })
                 .catch((err) => {
@@ -331,6 +332,8 @@ const UserPage = () => {
                 });
             await getUserData({userId}).then((data) => {
                 setUser(() => data);
+                const isKorean = data.role !== 'EXCHANGE';
+                setIsKorean(() => isKorean);
                 console.log(data); // for debug
             })
                 .catch((err) => {
@@ -355,7 +358,7 @@ const UserPage = () => {
                 <div className='image-back-container'>
                     <img className='image-back' src={user.imgBack || '/basic_background.png'} alt="배경사진"/>
                 </div>
-                <MoveButton owner={idSame(commenterId, userId)} userName={user.userName}/>
+                <MoveButton owner={idSame(commenterId, userId)} isKoreanMe={isKoreanMe} userName={user.userName}/>
                 <div className="user-content-container">
                     <div className="image-prof-container">
                         <img className="image-prof" src={user.imgProf || basicProfileImage} alt="프로필사진"/>
@@ -363,9 +366,9 @@ const UserPage = () => {
                     <div className="profile-container">
                         <h2>{user.userName}</h2>
                         <span>{user.univ}</span>
-                        {(!idSame(commenterId, userId) || isKorean) && <p className={'user-star'}><FaStar className={'yellow-star'}/> {user.star}</p>}
-                        <p>{t("userPage.region")}: {user.region}</p>
-                        <p>{t("userPage.time")}: {user.time}</p>
+                        {(isKorean) && <p className={'user-star'}><FaStar className={'yellow-star'}/> {user.star}</p>}
+                        {(isKorean) && <p>{t("userPage.region")}: {user.region}</p>}
+                        {(isKorean) && <p>{t("userPage.time")}: {user.time}</p>}
                         <div className="hashtag-section">
                             {user.hashtags && user.hashtags.map((hashtag, i) => {
                                 return (
@@ -376,7 +379,7 @@ const UserPage = () => {
                             })}
                         </div>
                     </div>
-                    {(!idSame(commenterId, userId) || isKorean) && <>
+                    {(isKorean) && <>
                         <div className="selfPR-container">
                             <h3>{t("userPage.self_pr")}</h3>
                             <p className="selfPR">{user.description}</p>
